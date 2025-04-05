b5bfba617b4471c3662021755abf571849482729
Suryatej <148959141+SuryatejPonnapalli@users.noreply.github.com>
2025-04-06 03:50:35 +0530

    calculateDob

---
+++ utils/calculateDob.ts
@@ -0,0 +1,16 @@
+export default function calculateAge(dobString: string): number {
+  const [day, month, year] = dobString.split("-").map(Number);
+  const dob = new Date(year, month - 1, day); // JS months are 0-based
+  const today = new Date();
+
+  let age = today.getFullYear() - dob.getFullYear();
+  const hasHadBirthdayThisYear =
+    today.getMonth() > dob.getMonth() ||
+    (today.getMonth() === dob.getMonth() && today.getDate() >= dob.getDate());
+
+  if (!hasHadBirthdayThisYear) {
+    age--;
+  }
+
+  return age;
+}
